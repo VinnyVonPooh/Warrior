ded1a19f252bd039b15625c386b5f581d2fc419d
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-06-30 15:41:11 +0400

    10. Set Up Hero Camera

--- Source/Warrior/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Characters/WarriorHeroCharacter.h
@@ -6,14 +6,24 @@
 #include "WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-/**
- *
- */
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	AWarriorHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
 };

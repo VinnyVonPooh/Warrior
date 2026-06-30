902f104f94014074af9d63d83dffb30cd92899ff
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-06-30 18:07:04 +0400

    14. Binding Input

--- Source/Warrior/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Characters/WarriorHeroCharacter.cpp
@@ -1,13 +1,17 @@
 // Groundhog & Drake All Rights Reserved.
 
-
 #include "WarriorHeroCharacter.h"
 
+#include "EnhancedInputSubsystems.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Engine/LocalPlayer.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Warrior/WarriorDebugHelper.h"
+#include "Warrior/WarriorGameplayTags.h"
+#include "Warrior/Components/Input/WarriorInputComponent.h"
+#include "Warrior/DataAssets/Input/DataAsset_InputConfig.h"
 
 AWarriorHeroCharacter::AWarriorHeroCharacter()
 {
@@ -33,9 +37,58 @@ AWarriorHeroCharacter::AWarriorHeroCharacter()
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 }
 
+void AWarriorHeroCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	check(InputConfigDataAsset);
+
+	auto LocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+	auto Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+	check(Subsystem);
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
+
+	auto WarriorInputComponent = CastChecked<UWarriorInputComponent>(PlayerInputComponent);
+	WarriorInputComponent->BindNativeInputAction(InputConfigDataAsset, WarriorGameplayTags::InputTag_Move, ETriggerEvent::Triggered, this,
+												 &AWarriorHeroCharacter::Input_Move);
+
+	WarriorInputComponent->BindNativeInputAction(InputConfigDataAsset, WarriorGameplayTags::InputTag_Look, ETriggerEvent::Triggered, this,
+												 &AWarriorHeroCharacter::Input_Look);
+}
+
 void AWarriorHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
 	Debud::Print("Working");
+}
+
+void AWarriorHeroCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+	const FRotator MovementRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+
+	if (MovementVector.Y != 0.f) {
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+	}
+
+	if (MovementVector.X != 0.f) {
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AWarriorHeroCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+	if (LookAxisVector.Y != 0.f) {
+		AddControllerPitchInput(-LookAxisVector.Y);
+	}
+	
+	if (LookAxisVector.X != 0.f) {
+		AddControllerYawInput(LookAxisVector.X);
+	}
+
 }
--- Source/Warrior/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Characters/WarriorHeroCharacter.h
@@ -6,6 +6,8 @@
 #include "WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+struct FInputActionValue;
+class UDataAsset_InputConfig;
 class UCameraComponent;
 class USpringArmComponent;
 
@@ -18,12 +20,21 @@ public:
 	AWarriorHeroCharacter();
 
 protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void BeginPlay() override;
 
-public:
+private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+
 };

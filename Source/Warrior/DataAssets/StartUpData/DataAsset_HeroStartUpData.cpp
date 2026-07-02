bfb1d2040e914827c4f570048be4a661ad28732b
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-02 16:18:33 +0400

    32. Hero Ability Set. Заполнение UDataAsset_HeroStartUpData - добавлена функция для вызова GiveAbility для экипировки

--- Source/Warrior/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/Warrior/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -1,3 +1,29 @@
 // Groundhog & Drake All Rights Reserved.
 
 #include "DataAsset_HeroStartUpData.h"
+
+#include "Warrior/AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "Warrior/AbilitySystem/Abilities/WarriorGameplayAbility.h"
+
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const auto& AbilitySet : HeroStartUpAbilitySets) {
+		if (!AbilitySet.IsValid()) {
+			continue;
+		}
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}

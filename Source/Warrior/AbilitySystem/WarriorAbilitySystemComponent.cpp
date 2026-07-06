72698ac077c802739a65791b37a17d1d92d57e20
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-06 19:48:05 +0400

    40. Grant Weapon Ability. Подготовка к снятию топора. Функция GrantHeroWeaponAbilities, настройка сохранения параметров в GA_Hero_EquipAxe

--- Source/Warrior/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/Warrior/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -2,6 +2,9 @@
 
 #include "WarriorAbilitySystemComponent.h"
 
+#include "Warrior/AbilitySystem/Abilities/WarriorGameplayAbility.h"
+
+
 void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& InInputTag)
 {
 	if (!InInputTag.IsValid()) {
@@ -20,5 +23,27 @@ void UWarriorAbilitySystemComponent::OnAbilityInputReleased(const FGameplayTag& 
 {
 	if (!InInputTag.IsValid()) {
 		return;
+	}
+}
+
+void UWarriorAbilitySystemComponent::GrantHeroWeaponAbilities(const TArray<FWarriorHeroAbilitySet>& InDefaultWeaponAbilities,
+															  int32 ApplyLevel,
+															  TArray<FGameplayAbilitySpecHandle>& OutGrantedAbilitySpecHandles)
+{
+	if (InDefaultWeaponAbilities.IsEmpty()) {
+		return;
+	}
+
+	for (const auto& AbilitySet : InDefaultWeaponAbilities) {
+		
+		if (!AbilitySet.IsValid()) {
+			continue;
+		}
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+		OutGrantedAbilitySpecHandles.AddUnique(GiveAbility(AbilitySpec));
 	}
 }

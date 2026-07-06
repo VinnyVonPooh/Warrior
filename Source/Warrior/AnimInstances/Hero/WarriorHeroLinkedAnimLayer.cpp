5a6a93587f8a1d26ac38082844aebac62cd14db4
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-06 14:22:26 +0400

    37. Master Anim Layer. Слой и BS для бега с оружием

--- Source/Warrior/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
+++ Source/Warrior/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
@@ -1,3 +1,11 @@
 // Groundhog & Drake All Rights Reserved.
 
 #include "WarriorHeroLinkedAnimLayer.h"
+
+#include "WarriorHeroAnimInstance.h"
+#include "Components/SkeletalMeshComponent.h"
+
+UWarriorHeroAnimInstance* UWarriorHeroLinkedAnimLayer::GetHeroAnimInstance() const
+{
+	return Cast<UWarriorHeroAnimInstance>(GetOwningComponent()->GetAnimInstance());
+}
--- Source/Warrior/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
+++ Source/Warrior/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
@@ -6,11 +6,15 @@
 #include "Warrior/AnimInstances/WarriorBaseAnimInstance.h"
 #include "WarriorHeroLinkedAnimLayer.generated.h"
 
-/**
- *
- */
+
+class UWarriorHeroAnimInstance;
+
 UCLASS()
 class WARRIOR_API UWarriorHeroLinkedAnimLayer : public UWarriorBaseAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, meta = (BlueprintThreadSafe))
+	UWarriorHeroAnimInstance* GetHeroAnimInstance() const;
 };

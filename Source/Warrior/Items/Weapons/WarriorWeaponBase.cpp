985103dc760dba4121d90556b21f3d443af500fc
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-09 12:24:51 +0400

    63. On Weapon Begin Overlap. Функции оверлапа

--- Source/Warrior/Items/Weapons/WarriorWeaponBase.cpp
+++ Source/Warrior/Items/Weapons/WarriorWeaponBase.cpp
@@ -4,6 +4,8 @@
 
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "GameFramework/Pawn.h"
+#include "Warrior/WarriorDebugHelper.h"
 
 AWarriorWeaponBase::AWarriorWeaponBase()
 {
@@ -16,4 +18,34 @@ AWarriorWeaponBase::AWarriorWeaponBase()
 	WeaponCollisionBox->SetupAttachment(GetRootComponent());
 	WeaponCollisionBox->SetBoxExtent(FVector(20.f));
 	WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	WeaponCollisionBox->OnComponentBeginOverlap.AddUniqueDynamic(this, &ThisClass::OnCollisionBoxBeginOverlap);
+	WeaponCollisionBox->OnComponentEndOverlap.AddUniqueDynamic(this, &ThisClass::OnCollisionBoxEndOverlap);
+}
+
+void AWarriorWeaponBase::OnCollisionBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+													UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+													const FHitResult& SweepResult)
+{
+	auto WeaponOwningPawn = GetInstigator<APawn>();
+	check(WeaponOwningPawn);
+
+	if (auto* HitPawn = Cast<APawn>(OtherActor)) {
+		if (WeaponOwningPawn != HitPawn) {
+			Debud::Print(GetName() + TEXT("BeginOverlap") + HitPawn->GetName(), FColor::Green);
+		}
+	}
+}
+
+void AWarriorWeaponBase::OnCollisionBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+												  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	auto WeaponOwningPawn = GetInstigator<APawn>();
+	check(WeaponOwningPawn);
+
+	if (auto* HitPawn = Cast<APawn>(OtherActor)) {
+		if (WeaponOwningPawn != HitPawn) {
+			Debud::Print(GetName() + TEXT("EndOverlap") + HitPawn->GetName(), FColor::Green);
+		}
+	}
 }

ca22bf204b55e01a78728f86ddc34febbdd1d3ca
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-23 14:30:49 +0400

    119. Activate Ability By Tag. Новый тип монстра - Dummy, он после того, как добежал активирует абилку атаки

--- Source/Warrior/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/Warrior/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -2,7 +2,7 @@
 
 #include "WarriorAbilitySystemComponent.h"
 
-#include "Warrior/AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "Warrior/AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
 
 void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& InInputTag)
 {
@@ -60,4 +60,23 @@ void UWarriorAbilitySystemComponent::RemoveGrantedHeroWeaponAbilities(UPARAM(ref
 	}
 
 	InSpecHandlesToRemove.Empty();
+}
+
+bool UWarriorAbilitySystemComponent::TryActivateAbilityByTag(FGameplayTag AbilityTagToActivate)
+{
+	check(AbilityTagToActivate.IsValid());
+
+	TArray<FGameplayAbilitySpec*> FoundAbilitySpecs;
+	GetActivatableGameplayAbilitySpecsByAllMatchingTags(AbilityTagToActivate.GetSingleTagContainer(), FoundAbilitySpecs);
+	
+	if (!FoundAbilitySpecs.IsEmpty()) {
+		const int32 RandomAbilityIndex = FMath::RandRange(0, FoundAbilitySpecs.Num() - 1);
+		auto* SpecToActivate = FoundAbilitySpecs[RandomAbilityIndex];
+		
+		check(SpecToActivate);
+		if (!SpecToActivate->IsActive()) {
+			return TryActivateAbility(SpecToActivate->Handle);
+		}
+	}
+	return false;
 }
--- Source/Warrior/AbilitySystem/WarriorAbilitySystemComponent.h
+++ Source/Warrior/AbilitySystem/WarriorAbilitySystemComponent.h
@@ -21,4 +21,7 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Warrior|Ability")
 	void RemoveGrantedHeroWeaponAbilities(UPARAM(ref) TArray<FGameplayAbilitySpecHandle>& InSpecHandlesToRemove);
+
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Ability")
+	bool TryActivateAbilityByTag(FGameplayTag AbilityTagToActivate);
 };

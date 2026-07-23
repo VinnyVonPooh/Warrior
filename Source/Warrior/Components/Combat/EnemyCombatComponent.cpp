fa3127cba4ee1ad23a8abe5b7c7c49c774103871
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-23 15:50:18 +0400

    121. Notify Melee Hit. Функция получения сигнала о уроне

--- Source/Warrior/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Components/Combat/EnemyCombatComponent.cpp
@@ -2,9 +2,38 @@
 
 #include "EnemyCombatComponent.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "Warrior/WarriorGameplayTags.h"
+
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	Super::OnHitTargetActor(HitActor);
+	if (OverlappedActors.Contains(HitActor)) {
+		return;
+	}
+
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO:
+	bool bIsValidBlock = false;
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+	
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable) {
+		//TODO: Check block
+		//bIsValidBlock = ...
+	}
+
+	FGameplayEventData Data;
+	Data.Instigator = GetOwningPawn();
+	Data.Target = HitActor;
+
+	if (bIsValidBlock) {
+		//TODO:
+		// block
+	} else {
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwningPawn(), WarriorGameplayTags::Shared_Event_MeleeHit, Data);
+	}
 }
 
 void UEnemyCombatComponent::OnWeaponPulledFromTargetActor(AActor* InteractedActor)

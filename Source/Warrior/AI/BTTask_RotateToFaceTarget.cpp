13c22dcc0caaf47e43ec75f27d07d061cc451fe7
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-23 18:45:45 +0400

    126. Construct Native BT Task. Новый таск - базовый код для такса вращения врага

---
+++ Source/Warrior/AI/BTTask_RotateToFaceTarget.cpp
@@ -0,0 +1,41 @@
+// Groundhog & Drake All Rights Reserved.
+
+#include "BTTask_RotateToFaceTarget.h"
+
+#include "GameFramework/Actor.h"
+
+UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
+{
+	NodeName = "Native Rotate to Face Target Actor";
+	AnglePrecision = 10.f;
+	RotationInterpSpeed = 5.f;
+
+	bNotifyTick = true;
+	bNotifyTaskFinished = true;
+	bCreateNodeInstance = false;
+
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+
+	InTargetToFaceKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass, InTargetToFaceKey), AActor::StaticClass());
+}
+
+void UBTTask_RotateToFaceTarget::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	if (auto* BBAsset = GetBlackboardAsset()) {
+		InTargetToFaceKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+uint16 UBTTask_RotateToFaceTarget::GetInstanceMemorySize() const
+{
+	return sizeof(FRotateToFaceTargetTaskMemory);
+}
+
+FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
+{
+	const FString KeyDescription = InTargetToFaceKey.SelectedKeyName.ToString();
+	return FString::Printf(TEXT("Smoothly rotates to face %s Key until the angle precision %s is reached"), *KeyDescription,
+						   *FString::SanitizeFloat(AnglePrecision));
+}

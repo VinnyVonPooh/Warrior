617097c5e073b6c7ad03284864df5b14b3034283
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-27 13:54:02 +0400

    140. Get Value At Level. Задание длины переката через таблицу значений

--- Source/Warrior/WarriorFunctionLibrary.h
+++ Source/Warrior/WarriorFunctionLibrary.h
@@ -8,6 +8,7 @@
 #include "WarriorTypes/WarriorEnumTypes.h"
 #include "WarriorFunctionLibrary.generated.h"
 
+struct FScalableFloat;
 class UPawnCombatComponent;
 class UWarriorAbilitySystemComponent;
 
@@ -39,4 +40,7 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "Warrior|FunctionLibrary")
 	static bool IsTargetPawnHostile(APawn* QueryPawn, APawn* TargetPawn);
+
+	UFUNCTION(BlueprintPure, Category = "Warrior|FunctionLibrary", meta = (CompactNodeTitle = "Get Value At Level"))
+	static float GetScalableFloatValueAtLevel(const FScalableFloat& InScalableFloat, float InLevel = 1.f);
 };

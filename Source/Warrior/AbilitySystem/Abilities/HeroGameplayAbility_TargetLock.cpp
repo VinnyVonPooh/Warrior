7c8401973346fc700dd652131472f199d116792e
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-28 15:37:01 +0400

    160. Get Available Targets. Функция для поиска врагов, находящихся в заданном боксе персонажа

--- Source/Warrior/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
+++ Source/Warrior/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.cpp
@@ -1,16 +1,57 @@
 // Groundhog & Drake All Rights Reserved.
 
-
 #include "HeroGameplayAbility_TargetLock.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+#include "Warrior/WarriorDebugHelper.h"
+#include "Warrior/Characters/WarriorHeroCharacter.h"
+
 void UHeroGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+													  const FGameplayAbilityActivationInfo ActivationInfo,
+													  const FGameplayEventData* TriggerEventData)
 {
+	TryLockOnTarget();
+
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
 void UHeroGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+												 const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility,
+												 bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UHeroGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+}
+
+void UHeroGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+	TArray<FHitResult> BoxTraceHits;
+
+	const auto* HeroCharacter = GetHeroCharacterFromActorInfo();
+	UKismetSystemLibrary::BoxTraceMultiForObjects(HeroCharacter,					 //
+												  HeroCharacter->GetActorLocation(), //
+												  HeroCharacter->GetActorLocation() +
+													  HeroCharacter->GetActorForwardVector() * BoxTraceDistance,				   //
+												  TraceBoxSize / 2.f,															   //
+												  HeroCharacter->GetActorForwardVector().ToOrientationRotator(),				   //
+												  BoxTraceChannel,																   //
+												  false,																		   //
+												  TArray<AActor*>(),															   //
+												  bShowPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None, //
+												  BoxTraceHits,																	   //
+												  true);
+
+	for (const auto& TraceHit : BoxTraceHits) {
+		if (AActor* HitActor = TraceHit.GetActor()) {
+			if (HitActor != HeroCharacter) {
+				AvailableActorsToLock.AddUnique(HitActor);
+
+				Debug::Print(HitActor->GetActorNameOrLabel());
+			}
+		}
+	}
 }

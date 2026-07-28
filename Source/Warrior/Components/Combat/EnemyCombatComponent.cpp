5ca40ed2593595862399b2a98d3b9ec8734fc03b
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-07-28 11:17:02 +0400

    153. Handle Successful Block. Доп эффекты на блок - поворот и отъезд героя, ниагара и звук на щите

--- Source/Warrior/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Components/Combat/EnemyCombatComponent.cpp
@@ -30,7 +30,7 @@ void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 	Data.Target = HitActor;
 
 	if (bIsValidBlock) {
-		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwningPawn(), WarriorGameplayTags ::Player_Event_SuccessfulBlock, Data);
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(HitActor, WarriorGameplayTags ::Player_Event_SuccessfulBlock, Data);
 	} else {
 		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwningPawn(), WarriorGameplayTags::Shared_Event_MeleeHit, Data);
 	}

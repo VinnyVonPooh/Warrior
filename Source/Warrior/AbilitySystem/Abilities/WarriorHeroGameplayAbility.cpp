91f4fad8f7c322b604f50859bba0e066412b0e50
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-08-06 18:59:41 +0400

    249. Resume Cooldown. Восстановление кулдауна после переикипировки оружия

--- Source/Warrior/AbilitySystem/Abilities/WarriorHeroGameplayAbility.cpp
+++ Source/Warrior/AbilitySystem/Abilities/WarriorHeroGameplayAbility.cpp
@@ -50,3 +50,19 @@ FGameplayEffectSpecHandle UWarriorHeroGameplayAbility::MakeHeroDamageEffectSpecH
 
 	return EffectSpecHandle;
 }
+
+bool UWarriorHeroGameplayAbility::GetAbilityRemainingCooldownByTag(FGameplayTag InCooldownTag, float& TotalCooldownTime,
+	float& RemainingCooldownTime)
+{
+	check(InCooldownTag.IsValid());
+
+	auto CooldownQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(InCooldownTag.GetSingleTagContainer());
+	auto TimeRemainingAndDuration = GetAbilitySystemComponentFromActorInfo()->GetActiveEffectsTimeRemainingAndDuration(CooldownQuery);
+	
+	if (!TimeRemainingAndDuration.IsEmpty()) {
+		RemainingCooldownTime = TimeRemainingAndDuration[0].Key;
+		TotalCooldownTime = TimeRemainingAndDuration[0].Value;
+	}
+
+	return RemainingCooldownTime > 0.f;
+}

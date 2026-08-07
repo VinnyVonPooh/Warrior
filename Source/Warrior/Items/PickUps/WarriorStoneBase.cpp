5c33b3a1ece4792feb4d2131e426070c2a6e6b9f
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-08-07 13:45:50 +0400

    259. On Stone Begin Overlap. Функция оверлапа камней, запускающая абилку сбора

--- Source/Warrior/Items/PickUps/WarriorStoneBase.cpp
+++ Source/Warrior/Items/PickUps/WarriorStoneBase.cpp
@@ -3,3 +3,15 @@
 
 #include "WarriorStoneBase.h"
 
+#include "Warrior/WarriorGameplayTags.h"
+#include "Warrior/AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "Warrior/Characters/WarriorHeroCharacter.h"
+
+void AWarriorStoneBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (auto* OverlappedHeroCharacter = Cast<AWarriorHeroCharacter>(OtherActor)) {
+		OverlappedHeroCharacter->GetWarriorAbilitySystemComponent()->TryActivateAbilityByTag(
+			WarriorGameplayTags::Player_Ability_PickUp_Stones);
+	}
+}
--- Source/Warrior/Items/PickUps/WarriorStoneBase.h
+++ Source/Warrior/Items/PickUps/WarriorStoneBase.h
@@ -6,10 +6,13 @@
 #include "WarriorPickUpBase.h"
 #include "WarriorStoneBase.generated.h"
 
-
 UCLASS()
 class WARRIOR_API AWarriorStoneBase : public AWarriorPickUpBase
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+													 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+													 const FHitResult& SweepResult) override;
 };

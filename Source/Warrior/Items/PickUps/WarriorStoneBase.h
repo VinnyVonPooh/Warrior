00e0f573bc5ac58377ead739c799ed1e57d3042d
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-08-07 11:46:13 +0400

    256. Stone Class. Базовый класс для пик апа

---
+++ Source/Warrior/Items/PickUps/WarriorStoneBase.h
@@ -0,0 +1,15 @@
+// Groundhog & Drake All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarriorPickUpBase.h"
+#include "WarriorStoneBase.generated.h"
+
+
+UCLASS()
+class WARRIOR_API AWarriorStoneBase : public AWarriorPickUpBase
+{
+	GENERATED_BODY()
+	
+};

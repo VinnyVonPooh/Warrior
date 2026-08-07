f7955c40b3d6160e0f18fa742616a121deb1322d
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-08-07 14:54:32 +0400

    261. Consume Stone Input. Настройка инпута для сбора камней

--- Source/Warrior/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Characters/WarriorHeroCharacter.h
@@ -48,6 +48,7 @@ private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
 	UDataAsset_InputConfig* InputConfigDataAsset;
 
+	UPROPERTY()
 	FVector2D SwitchDirection = FVector2D::ZeroVector;
 
 	void Input_Move(const FInputActionValue& InputActionValue);
@@ -55,6 +56,8 @@ private:
 
 	void Input_SwitchTargetTriggered(const FInputActionValue& InputActionValue);
 	void Input_SwitchTargetCompleted(const FInputActionValue& InputActionValue);
+
+	void Input_PickUpStonesStarted(const FInputActionValue& InputActionValue);
 
 	void Input_AbilityInputPressed(FGameplayTag InInputTag);
 	void Input_AbilityInputReleased(FGameplayTag InInputTag);

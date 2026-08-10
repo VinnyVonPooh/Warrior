0175e7a285fd08a0cc1fa4951de1b18d03c97963
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-08-10 16:31:12 +0400

    276. Wave Progression Logic. Логика перехода между состояниями волн

--- Source/Warrior/GameModes/WarriorSurvivalGameMode.cpp
+++ Source/Warrior/GameModes/WarriorSurvivalGameMode.cpp
@@ -6,15 +6,60 @@
 void AWarriorSurvivalGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	check(EnemyWaveSpawnerDataTable);
+
+	SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState::WaitSpawnNewWave);
+
+	TotalWavesToSpawn = EnemyWaveSpawnerDataTable->GetRowNames().Num();
 }
 
 void AWarriorSurvivalGameMode::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	if (CurrentSurvivalGameModeState == EWarriorSurvivalGameModeState::WaitSpawnNewWave) {
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= SpawnNewWaveWaitTime) {
+			TimePassedSinceStart = 0.f;
+			SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState::SpawningNewWave);
+		}
+	}
+	if (CurrentSurvivalGameModeState == EWarriorSurvivalGameModeState::SpawningNewWave) {
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= SpawnEnemiesDelayTime) {
+			//TODO
+
+			TimePassedSinceStart = 0.f;
+			SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState::InProgress);
+		}
+	}
+
+	if (CurrentSurvivalGameModeState == EWarriorSurvivalGameModeState::WaveCompleted) {
+		TimePassedSinceStart += DeltaSeconds;
+
+		if (TimePassedSinceStart >= WaveCompletedWaitTime) {
+			TimePassedSinceStart = 0.f;
+
+			CurrentWaveCount++;
+			if (HasFinishedAllWaves()) {
+				SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState::AllWavesDone);
+			} else {
+				SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState::WaitSpawnNewWave);
+			}
+		}
+	}
 }
 
 void AWarriorSurvivalGameMode::SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState InState)
 {
 	CurrentSurvivalGameModeState = InState;
 	OnSurvivalGameModeStateChanged.Broadcast(CurrentSurvivalGameModeState);
+}
+
+bool AWarriorSurvivalGameMode::HasFinishedAllWaves() const
+{
+	return CurrentWaveCount >= TotalWavesToSpawn;
 }
--- Source/Warrior/GameModes/WarriorSurvivalGameMode.h
+++ Source/Warrior/GameModes/WarriorSurvivalGameMode.h
@@ -60,7 +60,7 @@ protected:
 
 private:
 	void SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState InState);
-
+	bool HasFinishedAllWaves() const;
 
 	UPROPERTY()
 	EWarriorSurvivalGameModeState CurrentSurvivalGameModeState;
@@ -70,4 +70,22 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
 	UDataTable* EnemyWaveSpawnerDataTable;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	int32 TotalWavesToSpawn;
+
+	UPROPERTY()
+	int32 CurrentWaveCount = 1;
+
+	UPROPERTY()
+	float TimePassedSinceStart = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	float SpawnNewWaveWaitTime = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	float SpawnEnemiesDelayTime = 2.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
+	float WaveCompletedWaitTime = 5.f;
 };
